17be39d547405493a8d2d6d14eb7cb9fdc8159c2
Gabriel Balduíno <gabrielbalduino29@gmail.com>
2025-07-23 18:03:43 -0300

    Relacionamentos de muitos para muitos: categoria & imóveis

--- app/Http/Controllers/Api/RealStateController.php
+++ app/Http/Controllers/Api/RealStateController.php
@@ -45,6 +45,10 @@ class RealStateController extends Controller
 
             $realState = $this->realState->create($data); // Mass Asignment
 
+            if(isset($data['categories']) && count($data['categories'])) {
+                $realState->categories()->sync($data['categories']);
+            }
+
             return response()->json([
                 'data' => [
                     'msg' => 'Imóvel cadastrado com sucesso!'
@@ -70,6 +74,10 @@ class RealStateController extends Controller
             }*/
 
             $realState->update($data);
+
+            if(isset($data['categories']) && count($data['categories'])) {
+                $realState->categories()->sync($data['categories']);
+            }
 
             return response()->json([
                 'msg' => 'Imóvel atualizado com sucesso'
